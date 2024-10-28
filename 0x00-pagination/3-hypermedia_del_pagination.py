f52e69f5efe1e23f7afb082667cd5247442d2b3a
jeremymordecai628 <jeremymordecai08@gmail.com>
2024-10-28 10:34:29 +0300

    correction tsk0

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -3,7 +3,9 @@
 Helper function to calculate pagination range
 """
 
-def index_range(page, page_size):
+from typing import Tuple
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Calculate the start and end index for pagination.
 
